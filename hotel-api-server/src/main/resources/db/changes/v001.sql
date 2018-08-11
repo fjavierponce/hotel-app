01ae327a625612fef0899f68752349d354d762c2
fjavierponce <fco.poncecruz@gmail.com>
2018-08-11 00:56:59 -0500

    Added module for dao

--- hotel-api-server/src/main/resources/db/changes/v001.sql
+++ hotel-api-server/src/main/resources/db/changes/v001.sql
@@ -1,27 +1,34 @@
 CREATE TABLE HOTEL(
-   ID int not null,
-   NAME varchar(100),
-   PRIMARY KEY(ID)
-);
-
-CREATE TABLE ROOM_STATUS(
-   ID int not null,
-   STATUS varchar(50),
-   PRIMARY KEY(ID)
+    ID int not null,
+    NAME varchar(100),
+    CATEGORY int,
+    PRIMARY KEY(ID)
 );
 
 CREATE TABLE ROOM_TYPE(
-   ID int not null,
-   TYPE varchar(50),
-   PRIMARY KEY(ID)
+    ID int not null,
+    TYPE varchar(50),
+    ID_HOTEL int,
+    PRIMARY KEY(ID),
+    FOREIGN KEY(ID_HOTEL) REFERENCES HOTEL(ID)
 );
 
 CREATE TABLE ROOM(
-   ID int not null,
-   NAME varchar(100),
-   STATUS int,
-   TYPE int,
-   PRIMARY KEY(ID),
-   FOREIGN KEY (STATUS) REFERENCES ROOM_STATUS(ID),
-   FOREIGN KEY (TYPE) REFERENCES ROOM_TYPE(ID)
+    ID int not null,
+    NAME varchar(100),
+    STATUS varchar(50),
+    TYPE int,
+    ID_HOTEL int,
+    PRIMARY KEY(ID),
+    FOREIGN KEY(TYPE) REFERENCES ROOM_TYPE(ID),
+    FOREIGN KEY(ID_HOTEL) REFERENCES HOTEL(ID)
+);
+
+CREATE TABLE ROOM_TYPE_TARIFF(
+    ID int not null,
+    NUMBER_GUESTS int,
+    TARIFF NUMERIC(2),
+    ID_ROOM_TYPE int,
+    PRIMARY KEY(ID),
+    FOREIGN KEY(ID_ROOM_TYPE) REFERENCES ROOM_TYPE(ID)
 );
