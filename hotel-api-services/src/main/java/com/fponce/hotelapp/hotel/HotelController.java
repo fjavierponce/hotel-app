96886d54df2f689cb0dd239e0df6a17c0941775d
fjavierponce <fco.poncecruz@gmail.com>
2018-08-20 10:12:57 -0500

    Refactor changes for Controller

--- hotel-api-services/src/main/java/com/fponce/hotelapp/hotel/HotelController.java
+++ hotel-api-services/src/main/java/com/fponce/hotelapp/hotel/HotelController.java
@@ -6,10 +6,10 @@ import com.hotelapp.hotelapp.model.Hotel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
@@ -25,7 +25,7 @@ class HotelController {
         this.hotelService = hotelService;
     }
 
-    @PostMapping
+    @PostMapping(consumes = {MediaType.APPLICATION_JSON_VALUE})
     public ResponseEntity createHotel(@Valid @RequestBody Hotel hotel) {
         try {
             hotelService.createHotel(hotel);
