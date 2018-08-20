dcbad4b1f403aad3b51007e91492be0b65abba60
fjavierponce <fco.poncecruz@gmail.com>
2018-08-20 00:21:41 -0500

    Implemented equals, hashCode, toString

--- hotel-api-model/src/main/java/com/hotelapp/hotelapp/model/Hotel.java
+++ hotel-api-model/src/main/java/com/hotelapp/hotelapp/model/Hotel.java
@@ -44,4 +44,26 @@ public class Hotel {
     public void setCategory(int category) {
         this.category = category;
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Hotel hotel = (Hotel) obj;
+        return this.category == hotel.category && this.id.equals(hotel.id) && name.equals(hotel.name);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 17;
+        result = 31 * result + this.id.hashCode();
+        result = 31 * result + this.name.hashCode();
+        result = 31 * result + this.category;
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Hotel ID=%s, NAME=%s (%d stars)", this.id, this.name, this.category);
+    }
 }
