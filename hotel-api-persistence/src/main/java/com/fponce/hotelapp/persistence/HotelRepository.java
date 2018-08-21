841fdc295f599da1a5a738aad220be7926b74c62
fjavierponce <fco.poncecruz@gmail.com>
2018-08-21 00:36:21 -0500

    Added proper comments for classes.

--- hotel-api-persistence/src/main/java/com/fponce/hotelapp/persistence/HotelRepository.java
+++ hotel-api-persistence/src/main/java/com/fponce/hotelapp/persistence/HotelRepository.java
@@ -3,7 +3,18 @@ package com.fponce.hotelapp.persistence;
 import java.sql.SQLException;
 import java.util.UUID;
 
+/**
+ * Represents a repository class to persist Hotel entities. It allows access data from database,
+ * and create entities using JDBC.
+ */
 public interface HotelRepository {
 
+    /**
+     * Persists a hotel entity in database.
+     * @param id the hotel id
+     * @param name the hotel name
+     * @param category the hotel category
+     * @throws SQLException if there is an error when executing sql query.
+     */
     void createHotel(UUID id, String name, int category) throws SQLException;
 }
--- hotel-api-services/src/main/java/com/fponce/hotelapp/hotel/service/HotelService.java
+++ hotel-api-services/src/main/java/com/fponce/hotelapp/hotel/service/HotelService.java
@@ -3,7 +3,17 @@ package com.fponce.hotelapp.hotel.service;
 import com.fponce.hotelapp.exception.HotelAppServicesException;
 import com.hotelapp.hotelapp.model.Hotel;
 
+/**
+ * Represents a service class to manage hotel entities. This class allows to support hotels CRUD
+ * operations with business logic and rules.
+ * @author fponce
+ */
 public interface HotelService {
 
+    /**
+     * Validates and manages creation of a Hotel.
+     * @param hotel the hotel model that holds parameters.
+     * @throws HotelAppServicesException if there occurs an error calling repository.
+     */
     void createHotel(Hotel hotel) throws HotelAppServicesException;
 }
