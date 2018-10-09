2895892d1febcc3be30ca9c04cf256569556a963
Francisco Ponce <fco.poncecruz@gmail.com>
2018-10-08 17:40:49 -0700

    Endpoint for Get hotels in Controller

--- hotel-api-services/src/main/java/com/fponce/hotelapp/hotel/HotelController.java
+++ hotel-api-services/src/main/java/com/fponce/hotelapp/hotel/HotelController.java
@@ -6,14 +6,17 @@ import com.hotelapp.hotelapp.model.Hotel;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.Resources;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.Valid;
+import java.util.List;
 
 @RestController("v1/hotels")
 class HotelController {
@@ -26,9 +29,16 @@ class HotelController {
     }
 
     @PostMapping(consumes = {MediaType.APPLICATION_JSON_VALUE})
-    public ResponseEntity createHotel(@Valid @RequestBody Hotel hotel) throws HotelAppServicesException {
+    public Resource createHotel(@RequestBody Hotel hotel) throws HotelAppServicesException {
         logger.info("Calling createHotel with {}", hotel);
         hotelService.createHotel(hotel);
-        return ResponseEntity.status(HttpStatus.CREATED).build();
+        return new Resource(null);
+    }
+
+    @GetMapping
+    public Resources<Hotel> getHotels() throws HotelAppServicesException {
+        logger.info("Calling getHotels.");
+        List<Hotel> hotels = hotelService.getHotels();
+        return new Resources<>(hotels);
     }
 }
