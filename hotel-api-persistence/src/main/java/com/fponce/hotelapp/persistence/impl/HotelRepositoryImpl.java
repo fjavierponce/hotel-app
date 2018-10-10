803daa8db21d405e8ae6dbe359fd4fe802b952d1
Francisco Ponce <fco.poncecruz@gmail.com>
2018-10-10 12:32:49 -0700

    Fix for import wildcard

--- hotel-api-persistence/src/main/java/com/fponce/hotelapp/persistence/impl/HotelRepositoryImpl.java
+++ hotel-api-persistence/src/main/java/com/fponce/hotelapp/persistence/impl/HotelRepositoryImpl.java
@@ -6,7 +6,11 @@ import org.httprpc.sql.Parameters;
 import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
